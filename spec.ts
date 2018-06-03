6c5280b24eb06c9af54b0fe9bb5338919d1fe2b9
wlsf82 <wlsf82@gmail.com>
2018-06-03 18:18:32 +0200

    reorder element definition and use const instead of let

--- spec.ts
+++ spec.ts
@@ -4,13 +4,13 @@ describe("protractor with typescript typings", () => {
     beforeEach(() =>  browser.get("http://www.angularjs.org"));
 
     it("should greet the named user", () => {
+        const greeting = element(by.binding("yourName"));
         element(by.model("yourName")).sendKeys("Julie");
-        let greeting = element(by.binding("yourName"));
         expect(greeting.getText()).toEqual("Hello Julie!");
     });
 
     it("should list todos", () => {
-        let todoList = element.all(by.repeater("todo in todoList.todos"));
+        const todoList = element.all(by.repeater("todo in todoList.todos"));
         expect(todoList.count()).toEqual(2);
         expect(todoList.get(1).getText()).toEqual("build an AngularJS app");
     });
